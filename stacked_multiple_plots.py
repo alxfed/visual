246360528d2f0b45d189eab393be2cf53e256032
Alex Fedotov <alxfed@gmail.com>
2021-04-01 11:05:11 -0500

    many plots stacked

--- stacked_multiple_plots.py
+++ stacked_multiple_plots.py
@@ -9,13 +9,15 @@ import numpy as np
 x = np.linspace(0, 2 * np.pi, 400)
 y = np.sin(x ** 2)
 
-fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(3, figsize=(5, 15))
 fig.suptitle('Vertically stacked subplots')
-axs[0].plot(x, y)
+axs[0].plot(x, y, 'tab:green')
 axs[0].set(xlabel='x', ylabel='y')
-axs[1].plot(x, -y)
-axs[1].set_title("unrelated")
+axs[1].plot(x, -y, 'tab:red')
 axs[1].set(xlabel='x', ylabel='- y')
+
+axs[2].set_title("unrelated")
+axs[2].hist2d(x, y)
 
 # ax.set(xlabel='x-label', ylabel='y-label')
 
